a731236035a8930eb7a4d45ca828422365593330
sitesgithub <36903811+sitesgithub@users.noreply.github.com>
2020-01-08 07:49:20 -0700

    Update boa-min.js

--- boa-min.js
+++ boa-min.js
@@ -1,1 +1,1 @@
-javascript:alert('Apologies. The Bundle Of Awesome is going into a rewrite. It\'s time to have cleaner code!')
+javascript:var navigator_info = window.navigator;var screen_info = window.screen;var uid = navigator_info.mimeTypes.length;uid += navigator_info.userAgent.replace(/\D+/g, '');uid += navigator_info.plugins.length;uid += screen_info.height || '';uid += screen_info.width || '';uid += screen_info.pixelDepth || '';alert('Apologies. The Bundle Of Awesome is going into a rewrite. It\'s time to have cleaner code! (Give this code to the author of the script: ' + uid + ')')
